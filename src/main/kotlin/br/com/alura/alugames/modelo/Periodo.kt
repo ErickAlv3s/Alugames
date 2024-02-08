60353509923103415f6334e098c485ab3839b517
origin <erickalves1333@gmail.com>
2024-02-08 12:37:50 -0300

    alugames

---
+++ src/main/kotlin/br/com/alura/alugames/modelo/Periodo.kt
@@ -0,0 +1,13 @@
+package br.com.alura.alugames.modelo
+
+import java.time.LocalDate
+import java.time.Period
+import javax.persistence.Embeddable
+import javax.persistence.Embedded
+
+@Embeddable
+data class Periodo(
+    val dataInicial: LocalDate = LocalDate.now(),
+    val dataFinal: LocalDate = LocalDate.now().plusDays(7)) {
+    val emDias = Period.between(dataInicial, dataFinal).days
+}
